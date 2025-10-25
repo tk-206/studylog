2990bf45d665a3fb1e65370a08d1d5d237d2af0e
tk-206 <taekyoung206@gmail.com>
2025-10-25 23:10:15 +0900

    주석

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,6 +1,6 @@
 const STORAGE_KEY = "studylog_records";
 
-/* 데이터 저장 및 불러오기 유틸리티 함수 */
+/* 데이터 저장 */
 export const saveRecords = (records) => {
   try {
     if (!Array.isArray(records)) return;
@@ -9,7 +9,7 @@ export const saveRecords = (records) => {
     console.error("LocalStorage 저장 실패", e);
   }
 };
-
+/* 데이터 불러오기 */
 export const loadRecords = () => {
   try {
     const data = localStorage.getItem(STORAGE_KEY);
